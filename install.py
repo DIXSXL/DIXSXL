8acf2468fa90ded793ba90cc67c6f0e37a7c9cbb
DIXSXL <dimonle2205@gmail.com>
2024-08-21 00:04:16 +0300

    Create install.py

---
+++ install.py
@@ -0,0 +1,29 @@
+import os
+
+
+
+
+
+os.system('pip install pystyle')
+
+
+
+os.system('pip install requests')
+
+
+
+os.system('pip install ipinfo')
+
+
+
+os.system('pip install requests')
+
+
+
+
+
+os.system('pip install colorama')
+
+
+
+os.system('python main.py')
