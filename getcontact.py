0326a70cb7443102e9789d0f12bd507c4f38c3a4
DIXSXL <dimonle2205@gmail.com>
2024-08-21 15:32:22 +0300

    Update getcontact.py

--- getcontact.py
+++ getcontact.py
@@ -1,26 +1,23 @@
-import requests
-from bs4 import BeautifulSoup
+import csv
 
-def find_word_on_page(word):
-    url = "https://sites.google.com/view/dbgetcontact"
-    response = requests.get(url)
+search_word = input("введите номер: ")
 
-    if response.status_code != 200:
-        print(pystyle.Colorate.Horizontal(Colors.blue_to_white, "ошибка при доступе к базе"))
-        return
+with open("getcontact.csv", "r") as f:
+    reader = csv.reader(f)
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+ 
+    for row in reader:
+        if search_word in row:
+       
+            found_line = ",".join(row)
 
-    found = False
-    for line in soup.stripped_strings:
-        if word in line:
-            print(pystyle.Colorate.Horizontal(Colors.blue_to_white, f"отчет по {word}\n\n{line}"))
-            found = True
+         
+            print(f"""
+отчет по {search_word}
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┣ {found_line}
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━""")
             break
-
-    if not found:
-        print("ничего не найдено.")
-
-if __name__ == "__main__":
-    user_input = input(pystyle.Colorate.Horizontal(Colors.blue_to_white, "введите номер телефона: "))
-    find_word_on_page(user_input)
+    else:
+        
+        print(f"по запросу {search_word} ничего не найдено")
